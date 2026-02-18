46936c0c368e82d4d745bb15068d8e9a457a56f2
Vera-Sky-Harmony <vera.sky.harmony@gmail.com>
2026-02-18 10:22:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,21 @@ app.post("/webhook", express.raw({ type: "*/*" }), async (req, res) => {
       if (ev.type === "message" && ev.message.type === "text") {
         const text = ev.message.text.trim();
 
-        /* ===== Day7-2 ===== */
+        /* =======================
+           Day7-2
+        ======================== */
         if (text === "登録希望") {
+
+          // 画像送信
+          await client.pushMessage(userId, {
+            type: "image",
+            originalContentUrl:
+              "https://res.cloudinary.com/dxegzwukb/image/upload/v1771291127/X41_s9psh6.png",
+            previewImageUrl:
+              "https://res.cloudinary.com/dxegzwukb/image/upload/v1771291127/X41_s9psh6.png",
+          });
+
+          // 従来案内文
           await client.pushMessage(userId, {
             type: "text",
             text:
@@ -49,10 +62,13 @@ app.post("/webhook", express.raw({ type: "*/*" }), async (req, res) => {
               "準備ができましたら\n" +
               "『3点返信開始』と送信してください。",
           });
+
           continue;
         }
 
-        /* ===== 3点開始 ===== */
+        /* =======================
+           3点開始
+        ======================== */
         if (text === "3点返信開始") {
           threePointsState.set(userId, { step: 1 });
 
@@ -79,13 +95,36 @@ app.post("/webhook", express.raw({ type: "*/*" }), async (req, res) => {
         if (state?.step === 2) {
           state.flp = text;
 
+          // Day7-3 Flexメッセージ
           await client.pushMessage(userId, {
-            type: "text",
-            text:
-              "登録情報を受け付けました。\n\n" +
-              `氏名：${state.name}\n` +
-              `FLP番号：${state.flp}\n\n` +
-              "確認後、VSH譲渡手続きを進めます。",
+            type: "flex",
+            altText: "登録完了をLINEで返信する",
+            contents: {
+              type: "bubble",
+              hero: {
+                type: "image",
+                url:
+                  "https://res.cloudinary.com/dxegzwukb/image/upload/v1769679233/Day7-1_dpjx3u.png",
+                size: "full",
+                aspectRatio: "1:1",
+                aspectMode: "cover",
+              },
+              body: {
+                type: "box",
+                layout: "vertical",
+                contents: [
+                  {
+                    type: "button",
+                    style: "primary",
+                    action: {
+                      type: "message",
+                      label: "登録完了をLINEで返信する",
+                      text: "登録完了",
+                    },
+                  },
+                ],
+              },
+            },
           });
 
           threePointsState.delete(userId);
