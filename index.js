be72ae0cfa1830f20b6595c767b41c7fac60ced4
Vera-Sky-Harmony <vera.sky.harmony@gmail.com>
2026-08-19 15:39:45 +0900

    Remove console log for ADMIN_FILE
    Remove console log for reading ADMIN_FILE.

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ catch (err) {
 app.get("/api/next-flp", async (_req, res) => {
 console.log("===== /api/next-flp =====");
 console.log("ADMIN_FILE =", ADMIN_FILE);
-console.log(fs.readFileSync(ADMIN_FILE, "utf8"));
+
   const data = await loadAdmin();
 
   const item = data.flpList.find(
