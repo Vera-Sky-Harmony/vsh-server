9c7ca1287c6dbdf4185faa9d932a26c28f4636bf
Vera-Sky-Harmony <vera.sky.harmony@gmail.com>
2026-08-19 14:09:48 +0900

    Add pushToIntroducer function to send messages

--- message.js
+++ message.js
@@ -1,1 +1,16 @@
+export async function pushToIntroducer(client, introducerUserId) {
 
+  if (!introducerUserId) return;
+
+  await client.pushMessage(introducerUserId, {
+    type: "text",
+    text:
+`登録を受け付けました。
+
+紹介者がFLP本体システムで登録を確認後、
+VSHを譲渡いたします。
+
+Day7-3`
+  });
+
+}
