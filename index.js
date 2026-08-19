7b0d546ec0a3887e7c13834f506bff03e6023593
Vera-Sky-Harmony <vera.sky.harmony@gmail.com>
2026-08-19 15:04:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -243,9 +243,7 @@ console.log(fs.readFileSync(ADMIN_FILE, "utf8"));
 
 app.post("/api/use-flp", async (req, res) => {
 
-  const data = JSON.parse(
-    fs.readFileSync(ADMIN_FILE, "utf8")
-  );
+const data = await loadAdmin(); 
 
   const item = data.flpList.find(
     x => x.flp === req.body.flp
