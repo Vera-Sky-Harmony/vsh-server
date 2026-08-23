6d88f713501c81a0052702f64c437cb621c0a267
Vera-Sky-Harmony <vera.sky.harmony@gmail.com>
2026-08-23 20:37:08 +0900

    Modify image type and update URLs in index.js
    Updated image URLs for the member's content.

--- index.js
+++ index.js
@@ -436,14 +436,15 @@ app.post("/api/confirm-member", async (req, res) => {
   [
 
     {
-      type: "image",
+    {
+  type: "image",
 
-      originalContentUrl:
-        "https://res.cloudinary.com/dxegzwukb/image/upload/v1787477831/登録おめでとう_v6xuwt.png",
+  originalContentUrl:
+    "https://res.cloudinary.com/dxegzwukb/image/upload/v1787477831/vsh-day8-congratulations",
 
-      previewImageUrl:
-        "https://res.cloudinary.com/dxegzwukb/image/upload/v1787477831/登録おめでとう_v6xuwt.png"
-    },
+  previewImageUrl:
+    "https://res.cloudinary.com/dxegzwukb/image/upload/v1787477831/vsh-day8-congratulations"
+},
 
     {
       type: "text",
