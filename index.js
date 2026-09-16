00547fea37fc079d075aa5794477bd352fd0c499
Vera-Sky-Harmony <vera.sky.harmony@gmail.com>
2026-09-16 20:43:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4079,13 +4079,13 @@ app.get("/vsh/invite/:flp", async (req, res) => {
     );
 
 
-    //----------------------------------
-    // Day0へ
-    //----------------------------------
+   //----------------------------------
+// VSH公式LINE友だち追加画面へ
+//----------------------------------
 
-    return res.redirect(
-      "/pages/day0.html"
-    );
+return res.redirect(
+  "/pages/invite.html"
+);
 
   }
 
